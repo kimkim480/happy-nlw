7923a32233e44d1ca30d37d66558683cf5b2c9ff
Eric <lordkim480@gmail.com>
2020-10-14 19:16:59 -0300

    Day 3 complete

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,30 @@
-import { ftruncate } from "fs";
-import React from "react";
-import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 import mapMarkerImg from "../images/map-marker.svg";
 
-import '../styles/pages/orphanages-map.css'
-import 'leaflet/dist/leaflet.css'
+import "../styles/pages/orphanages-map.css";
+import MapIcon from "../utils/mapIcon";
+import api from "../services/api";
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get("orphanages").then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -21,23 +36,46 @@ function OrphanagesMap() {
         </header>
 
         <footer>
-            <strong>Teófilo Otoni</strong>
-            <span>Minas Gerais</span>
+          <strong>Teófilo Otoni</strong>
+          <span>Minas Gerais</span>
         </footer>
       </aside>
 
-      <Map 
-      center={[-17.8926039,-41.5112767]} 
-      zoom={15}
-      style={{ width: '100%', height: '100%' }}
+      <Map
+        center={[-17.8926039, -41.5112767]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
       >
-         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+        {orphanages.map((orphanage) => {
+          return (
+            <Marker
+              icon={MapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+              key={orphanage.id}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxHeight={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#fff" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
 
-        <Link to="" className="create-orphanage">
-            <FiPlus size={32} color="#FFF"/>
-        </Link>
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
     </div>
   );
 }
